c6d98c2ffc46f35b377c1a1c4b5d6189f46586db
Irini Lygerou <irini.lyg@gmail.com>
2020-06-26 12:30:56 +0300

    Basic Espresso Tests added

---
+++ src/androidTest/java/de/tudarmstadt/informatik/hostage/ui/activity/ProfilesTest.java
@@ -0,0 +1,199 @@
+package de.tudarmstadt.informatik.hostage.ui.activity;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import de.tudarmstadt.informatik.hostage.R;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ProfilesTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void profilesTest() {
+        ViewInteraction switch_ = onView(
+                allOf(withId(R.id.home_switch_connection),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        switch_.perform(click());
+
+        ViewInteraction linearLayout = onView(
+                allOf(withContentDescription("Overview, Open navigation drawer"),
+                        childAtPosition(
+                                allOf(withClassName(is("com.android.internal.widget.ActionBarView")),
+                                        childAtPosition(
+                                                withClassName(is("com.android.internal.widget.ActionBarContainer")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        linearLayout.perform(click());
+
+        DataInteraction linearLayout2 = onData(anything())
+                .inAdapterView(allOf(withId(R.id.left_drawer),
+                        childAtPosition(
+                                withId(R.id.drawer_layout),
+                                1)))
+                .atPosition(5);
+        linearLayout2.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.profile_manager_item_button_edit),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.swipelist_backview),
+                                        0),
+                                0),
+                        isDisplayed()));
+        imageButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction listView = onView(
+                allOf(withId(android.R.id.list),
+                        childAtPosition(
+                                allOf(withId(android.R.id.list_container),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        listView.check(matches(isDisplayed()));
+
+        DataInteraction linearLayout3 = onData(anything())
+                .inAdapterView(allOf(withId(android.R.id.list),
+                        childAtPosition(
+                                withId(android.R.id.list_container),
+                                0)))
+                .atPosition(22);
+        linearLayout3.perform(click());
+
+        ViewInteraction checkBox = onView(
+                allOf(withId(android.R.id.checkbox),
+                        childAtPosition(
+                                allOf(withId(android.R.id.widget_frame),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                2)),
+                                0),
+                        isDisplayed()));
+        checkBox.check(matches(isDisplayed()));
+
+        ViewInteraction linearLayout4 = onView(
+                allOf(withId(R.id.action_done),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.android.internal.widget.ActionBarView")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        linearLayout4.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction imageButton2 = onView(
+                allOf(withId(R.id.profile_manager_item_button_edit),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.swipelist_backview),
+                                        0),
+                                0),
+                        isDisplayed()));
+        imageButton2.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction linearLayout5 = onView(
+                allOf(withId(R.id.action_cancel),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.android.internal.widget.ActionBarView")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        linearLayout5.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
