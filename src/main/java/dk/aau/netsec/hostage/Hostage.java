f0388977e13a22811d3d560334d56b4a72b18075
Afsaneh <a.gheisariyeh25@gmail.com>
2025-06-19 11:53:47 +0200

    Implemented final version of the BACnet protocol

--- src/main/java/dk/aau/netsec/hostage/Hostage.java
+++ src/main/java/dk/aau/netsec/hostage/Hostage.java
@@ -500,6 +500,7 @@ public class Hostage extends Service {
     private Listener createListener(String protocolName, int port) {
         for (Protocol protocol : implementedProtocols) {
             if (protocolName.equals(protocol.toString())) {
+                /*
                 if (protocolName.equals("MQTT")) {
                     return addMQTTListener(protocol, port);
                 }
@@ -508,7 +509,7 @@ public class Hostage extends Service {
                 }
                 if (protocolName.equals("AMQP")) {
                     return addAMQPListener(protocol, port);
-                }
+                }*/
                 Listener listener = new Listener(this, protocol, port);
                 listeners.add(listener);
                 return listener;
